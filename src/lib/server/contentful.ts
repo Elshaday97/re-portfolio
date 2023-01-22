493a65d4c1520f3c7029a251ef2bae91b51242a3
Elshaday97 <yirga.elshaday@gmail.com>
2023-01-22 14:20:51 +0300

    fix: content entry fix

--- src/lib/server/contentful.ts
+++ src/lib/server/contentful.ts
@@ -1,19 +1,24 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import contentful, { type ContentfulClientApi, type EntryCollection } from 'contentful';
-import { PRIVATE_SPACE_ID, PRIVATE_ACCESS_TOKEN, PRIVATE_ENTRY_KEY } from '$env/static/private';
+import { PRIVATE_SPACE_ID, PRIVATE_ACCESS_TOKEN } from '$env/static/private';
 import type { IContent } from './types';
 
 const contentfulClient: ContentfulClientApi = contentful.createClient({
 	space: PRIVATE_SPACE_ID,
 	environment: 'master', // defaults to 'master' if not set
-	accessToken: PRIVATE_ACCESS_TOKEN
+	accessToken: PRIVATE_ACCESS_TOKEN,
+	resolveLinks: true
 });
 
 const getContents = async () => {
 	try {
-		const response: EntryCollection<any> = await contentfulClient.getEntries(PRIVATE_ENTRY_KEY);
-		if (response && response.items && response.items.length > 1) {
-			const content: IContent | undefined = response.items[6]?.fields;
+		const response: EntryCollection<any> = await contentfulClient.getEntries({
+			content_type: 'mainPage',
+			include: 10
+		});
+		if (response && response.items && response.items.length > 0) {
+			const content: IContent | undefined = response.items[0]?.fields;
+
 			return content;
 		}
 	} catch (error) {
